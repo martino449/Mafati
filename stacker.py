6bbb6db91179f68494bfc7ec600757c411fdeabb
Mario Pisano <76888581+martino449@users.noreply.github.com>
2024-09-26 22:11:57 +0200

    Update stacker.py

--- stacker.py
+++ stacker.py
@@ -245,7 +245,7 @@ class Stack:
         """Print information about Stacker, including the version number and
         copyright/license information."""
 
-        print('Stacker version 1.3')
+        print('Stacker version 1.4')
         print('Copyright (C) 2024 Mario Pisano')
         print('under the MIT License')
     def _execute_function(self, func_name: str) -> None:
